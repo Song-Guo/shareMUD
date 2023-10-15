5ae7fd5c71bbdc4728bc5c8aba14b3634edb3be9
Minzhao Lyu <z5025089@L-G17NMMC2WX.local>
2023-10-16 09:51:04 +1100

    cleaned repository

--- Archived_Ver/autorun.sh
+++
@@ -1,7 +0,0 @@
-python3 Sim_shareMUD-6.py >> runtimelog0.txt
-python3 Sim_shareMUD-6-case1.py >> runtimelog1.txt
-python3 Sim_shareMUD-6-case2.py >> runtimelog2.txt
-python3 Sim_shareMUD-6-case3.py >> runtimelog3.txt
-python3 Sim_shareMUD-6-case4.py >> runtimelog4.txt
-python3 Sim_shareMUD-6-case5.py >> runtimelog5.txt
-python3 Sim_shareMUD-6-case6.py >> runtimelog6.txt
